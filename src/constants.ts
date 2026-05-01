c42ef09e91cda97434373dbdbc455b9f3d7f20fd
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2026-05-02 00:15:39 +0200

    feat: add resume

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const FIRST_NAME = "Thomas"
+export const MIDDLE_NAME = "Maximilian"
+export const LAST_NAME = "Schöller"
+
+export const FULL_NAME = `${FIRST_NAME} ${MIDDLE_NAME} ${LAST_NAME}`;
