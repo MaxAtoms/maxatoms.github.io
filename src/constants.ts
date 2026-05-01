1226d75004b0b8a0d47f3feaba2a673eca2debf9
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2026-05-02 00:36:26 +0200

    feat: update metadata

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,6 @@ export const FIRST_NAME = "Thomas"
 export const MIDDLE_NAME = "Maximilian"
 export const LAST_NAME = "Schöller"
 
+export const PROFESSION = "Software & Computer Engineer"
+
 export const FULL_NAME = `${FIRST_NAME} ${MIDDLE_NAME} ${LAST_NAME}`;
